d345a1a3d30b605454d3e0d5be7d817e62df17d0
JohanAppeltaart <72360650+JohanAppeltaart@users.noreply.github.com>
2021-02-08 20:09:21 +0100

    Update

--- src/main/java/com/johanappeltaart/choenk/Choenk.java
+++ src/main/java/com/johanappeltaart/choenk/Choenk.java
@@ -1,7 +1,10 @@
-package com.example.examplemod;
+package com.johanappeltaart.choenk;
 
+import com.johanappeltaart.choenk.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,13 +22,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("choenk")
+public class Choenk
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "choenk";
 
-    public ExampleMod() {
+    public Choenk() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -34,6 +38,9 @@ public class ExampleMod
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -50,6 +57,13 @@ public class ExampleMod
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
+
+    public static final ItemGroup TAB = new ItemGroup("choenkTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModItems.WOODEN_CHOENKAXE.get());
+        }
+    };
 
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
