f74cb749c320b72ab5ec0df7c95c7f0934467985
JohanAppeltaart <72360650+JohanAppeltaart@users.noreply.github.com>
2021-02-09 04:23:33 +0100

    works

--- src/main/java/com/johanappeltaart/choenk/init/ModItems.java
+++ src/main/java/com/johanappeltaart/choenk/init/ModItems.java
@@ -2,6 +2,7 @@ package com.johanappeltaart.choenk.init;
 
 import com.johanappeltaart.choenk.Choenk;
 import com.johanappeltaart.choenk.items.ModChoenkAxeItem;
+import com.johanappeltaart.choenk.items.ModVoidChoenkAxeItem;
 import com.johanappeltaart.choenk.tools.ModItemTier;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
@@ -15,6 +16,9 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Choenk.MOD_ID);
 
     public static final RegistryObject<ModChoenkAxeItem> WOODEN_CHOENKAXE = ITEMS.register("wooden_choenkaxe",
-            ()-> new ModChoenkAxeItem(ModItemTier.WOOD,1,-2.4f,new Item.Properties().group(Choenk.TAB).food(new Food.Builder().hunger(9).saturation(3.0f).effect(()->new EffectInstance(Effects.GLOWING,200),0.8f).effect(()->new EffectInstance(Effects.POISON,60),0.5f).build())));
+            ()-> new ModChoenkAxeItem(ModItemTier.WOOD,1,-2.4f, new Item.Properties().group(Choenk.TAB)));
+
+    public static final RegistryObject<ModVoidChoenkAxeItem> VOID_CHOENKAXE = ITEMS.register("void_choenkaxe",
+            ()-> new ModVoidChoenkAxeItem(ModItemTier.WOOD,1,-2.4f,new Item.Properties().group(Choenk.TAB)));
 
 }
